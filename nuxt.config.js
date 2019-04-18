cbf33f4aa8ba9a503ea71c895a4a2616966e3256
zhuang.li <zhuang.li@bitmain.com>
2019-04-18 10:36:39 +0800

    git precommit test

--- nuxt.config.js
+++ nuxt.config.js
@@ -79,6 +79,16 @@ module.exports = {
     },
     extend(config, ctx) {
       // console.log(config, '!!!!!!!!!!!!!!!!!!!', ctx)
+
+      // Run ESLint on save
+      if (ctx.isDev && ctx.isClient) {
+        config.module.rules.push({
+          enforce: 'pre',
+          test: /\.(js|vue)$/,
+          loader: 'eslint-loader',
+          exclude: /(node_modules)/,
+        });
+      }
     },
   },
 };
